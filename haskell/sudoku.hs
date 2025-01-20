c68b1c064df7040564e4965ba9457302c518f1e8
michaelhunter27 <michael.max.hunter27@gmail.com>
2025-01-20 15:40:12 -0800

    Improved solving (4x4)
    Add functions that determine possible cell values based on row, column
    and box placement rules.
    Add alternate solve function (solve') that uses recursion to loop
    through all the cells of the grid and fills in cells that have only
    one possible value.

--- haskell/sudoku.hs
+++ haskell/sudoku.hs
@@ -79,3 +79,64 @@ fillInMissingValue grid row values
     | otherwise = grid
     where index = head [x | x <- [0..3], (grid !! row) !! x == 0 ]
           value = head (missingValues (grid !! row) values)
+
+-- given a grid and the indexes of the row and col of a cell,
+-- returns the values that can go in an empty cell
+-- returns an empty list if the cell is not empty
+possibleValues :: (Num a, Eq a) => [[a]] -> Int -> Int -> [a] -> [a]
+possibleValues grid row col values
+    | (grid !! row !! col) == 0 = missingValues (grid !! row ++ gridCols grid !! col ++ gridBoxes grid !! box) values
+    | otherwise = []
+    where box = row `div` 2 * 2 + col `div` 2
+
+-- returns True if there is only one value that can go in the cell
+-- returns false if the cell is already filled
+onePossible :: (Num a, Eq a) => [[a]] -> Int -> Int -> [a] -> Bool
+onePossible grid row col values
+    | grid !! row !! col == 0 = length (possibleValues grid row col values) == 1
+    | otherwise = False
+
+-- returns a grid with the cell cell at row col filled in if there is only one value it could be
+fillInOnlyPossibleValue :: (Num a, Eq a) => [[a]] -> Int -> Int -> [a] -> [[a]]
+fillInOnlyPossibleValue grid row col values
+    | onePossible grid row col values = fillInValue grid row col value
+    | otherwise = grid
+    where value = head (possibleValues grid row col values)
+
+solve' :: (Num a, Eq a) => [[a]] -> [a] -> [[a]]
+solve' grid values
+    | isSolved grid values = grid
+    | otherwise = solveHelper grid 0 0 values
+
+
+-- uses recursive calls to loop through the cells of the sudoku
+-- fills in cells that have only one possible value
+solveHelper :: (Num a, Eq a) => [[a]] -> Int -> Int -> [a] -> [[a]]
+solveHelper grid row col values
+    | (col == size - 1) && (row == size - 1) = fillInOnlyPossibleValue grid row col values
+    | col == size - 1 = solveHelper (fillInOnlyPossibleValue grid row col values) (row + 1) 0 values
+    | otherwise = solveHelper (fillInOnlyPossibleValue grid row col values) row (col + 1) values
+    where size = length grid
+          
+-- empty grid for testing purposes
+grid00 = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+-- cell 1 1 can only be a 4
+grid05 = [[1,2,0,0],[3,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+-- cell 0 1 can only be a 2
+grid06 = [[1,0,0,0],[0,0,0,4],[0,0,0,0],[0,3,0,0]]
+
+-- one box missing, can be solved in one pass
+grid07 = [[0,0,3,4],[0,0,1,2],[2,3,4,1],[4,1,2,3]]
+
+-- two boxes missing, can be solved in one pass
+grid08 = [[0,0,3,4],[0,0,1,2],[2,3,0,0],[4,1,0,0]]
+
+-- two boxes and one cell missing, can be solved, but not in one pass
+grid09 = [[0,0,3,0],[0,0,1,2],[2,3,0,0],[4,1,0,0]]
+
+-- trickier? sudokus
+grid10 = [[0,1,0,0],[3,0,0,1],[4,0,0,2],[0,0,4,0]]
+grid11 = [[2,0,0,0],[0,0,0,4],[4,0,0,0],[0,0,0,3]]
+grid12 = [[0,0,0,1],[0,4,0,0],[0,0,3,0],[2,0,0,0]]
